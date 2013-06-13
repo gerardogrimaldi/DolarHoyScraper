2f10756b15218e93fe9ff2e072d43bd2cbec715c
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2013-06-12 23:39:20 -0300

    andando en c9

--- server.js
+++ server.js
@@ -1,35 +1,43 @@
-///http://stackoverflow.com/questions/9119648/securing-my-node-js-apps-rest-api///http://comments.gmane.org/gmane.comp.lang.javascript.nodejs/55287///http://stackoverflow.com/questions/16159063/how-to-secure-restful-route-in-backbone-and-express
-var mongoose = require ("mongoose"); 
-var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 
-// conexion a mongolabs 'mongodb://dolarhoy:Hola123!@ds051447.mongolab.com:51447/dolarhoydb';
-'mongodb://dolarhoy:Traserito#321!@widmore.mongohq.com:10010/dolarhoydb';
+var mongoose = require ("mongoose"); ///http://stackoverflow.com/questions/9119648/securing-my-node-js-apps-rest-api///http://comments.gmane.org/gmane.comp.lang.javascript.nodejs/55287///http://stackoverflow.com/questions/16159063/how-to-secure-restful-route-in-backbone-and-express
+var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://dolarhoy:Traserito#321!@widmore.mongohq.com:10010/dolarhoydb';
 var Crawler = require("crawler").Crawler;
 var express = require('express');
 var jquery = require('jquery');
+var path = require('path');
+var mail = require("./nodemail");
+var valoresSchema = require("./Model/mongoSchema").valoresDolarHoySchema;
+var Valores = mongoose.model('ValoresDolarHoy', valoresSchema);
 var compraDolar;
 var ventaDolar;
 var compraEuro;
 var ventaEuro;
-var mail = require("./nodemail");
-var valoresSchema = require("./Model/mongoSchema").valoresDolarHoySchema;
-var Valores = mongoose.model('ValoresDolarHoy', valoresSchema);
-var work = false;
-var offset = -3;
 var intervalTime = 900000;
+var work = false;
+var offset = 0;
 
 var app = express();
-app.use(express.logger());
-
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+    
 function main(){
     if (work) {
         var day = new Date( new Date().getTime() + offset * 3600 * 1000).getDay();//var day = new Date( new Date().getTime() + offset * 3600 * 1000).toUTCString().replace( / GMT$/, "" ).getDay();//;
         if(day !== 0 && day !== 6){
-            var hour = new Date( new Date().getUTCDate() + offset * 3600 * 1000).getHours() + 5;/*.toUTCString().replace( / GMT$/, "" ).getHours()*/
+            var hour = new Date( new Date().getUTCDate() + offset * 3600 * 1000).getHours() + 4;/*.toUTCString().replace( / GMT$/, "" ).getHours()*/
             console.log(hour);
             if(hour >= 9 && hour <= 18){
                 console.log('Working...');
                 try { worker(); }
-                catch (Err){ onError(Err); }
+                catch (Err) { onError(Err); }
             } else { console.log('Not Working hours...'); }
         } else { console.log('No Working Weekday...'); }
     }
@@ -42,7 +50,7 @@ function worker(){
         "uri":"http://ambito.com/economia/mercados/monedas/dolar/",
         "jQuery":false,
         "callback":function(error,result) {
-            if(error && error.response.statusCode !== 200){console.log('Request error.');}
+            if(error && error.response.statusCode !== 200) { console.log('Request error.'); }
             if(result.body.length > 0){
                 getValuesDolar(result.body.toString());
                 console.log("Grabbed Dolar ", result.body.length, "bytes");
@@ -53,7 +61,7 @@ function worker(){
         "uri":"http://ambito.com/economia/mercados/monedas/euro/",
         "jQuery":false,
         "callback":function(error,result) {
-            if(error && error.response.statusCode !== 200){console.log('Request error.');}
+            if(error && error.response.statusCode !== 200) { console.log('Request error.'); }
             if(result.body.length > 0){
                 getValuesEuro(result.body.toString());
                 console.log("Grabbed Euro ", result.body.length, "bytes");
@@ -91,18 +99,16 @@ function getValuesEuro(resultString){
 function saveVals(){
     try{
         if (compraDolar !== undefined && ventaDolar !== undefined && compraEuro !== undefined && ventaEuro !== undefined){
-            
             var dolarTarjeta;
             var valoresDolarHoyObj;
             var dateBA = new Date( new Date().getTime() + offset * 3600 * 1000).toUTCString().replace( / GMT$/, "" );
-            
+            console.log(dateBA);
             dolarTarjeta = parseFloat(ventaDolar[0].replace(",",".")) + (parseFloat(ventaDolar[0].replace(",",".")) * 20 /100);
             dolarTarjeta = dolarTarjeta.toFixed(3);
             mongoose.connect(uristring, function (err, res) {
                 if (err) { console.log ('ERROR connecting to: ' + uristring + '. ' + err); } 
                 else { console.log ('Succeeded connected to: ' + uristring); }
             });
-    
             valoresDolarHoyObj = new Valores ({
                 dolarCompra : compraDolar[0].replace(",","."),
                 dolarVenta : ventaDolar[0].replace(",","."),
@@ -114,7 +120,6 @@ function saveVals(){
                 date : dateBA
             });
             
- 
             Valores.findOne()
             .select('dolarCompra dolarVenta dolarBlueCompra dolarBlueVenta dolarTarjeta euroCompra euroVenta date')
             .sort('-date')
@@ -127,18 +132,19 @@ function saveVals(){
                     doc.dolarBlueVenta != valoresDolarHoyObj.dolarBlueVenta ||
                     doc.euroCompra != valoresDolarHoyObj.euroCompra ||
                     doc.euroVenta != valoresDolarHoyObj.euroVenta ) {
-                    valoresDolarHoyObj.save( function (err) { 
-                        if (err) { onError ('Error on save!'); }
-                        else { onError ('Saved!'); }
-                    }
+                    valoresDolarHoyObj.save(
+                        function (err) { 
+                            if (err) { onError('Error on save!'); }
+                            else { console.log ('Saved!'); }
+                            }
                     )} 
                 });
+            compraDolar = undefined;
+            ventaDolar = undefined;
+            compraEuro = undefined;
+            ventaEuro = undefined; 
         }
-        compraDolar = undefined;
-        ventaDolar = undefined;
-        compraEuro = undefined;
-        ventaEuro = undefined;
-        }    
+    }
     catch(err){ onError(err); }
     //finally { mongoose.connection.close();}
 }
@@ -149,6 +155,10 @@ function onError(err) {
     mail.sendMail();
     console.log(err);
 }
+
+app.get('/',function(req, res){
+  res.render('index', { title: 'Express' });
+});
 
 app.get('/start/:pass', function(req, res) {
     if(req.params.pass != 'Hola123!'){return res.send('Error: Wrong password...');}
